4fae5b35d2578ffbea0a257842cdc1c63d3975ae
Jari Poutanen <jj.poutanen@gmail.com>
2020-10-11 14:09:14 +0300

    5.7 ok

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import React, { useState, useEffect } from 'react'
 import Blog from './components/Blog'
-import blogService from './services/blogs'
 import LoginForm from './components/LoginForm'
 import Togglable from './components/Togglable'
 import loginService from './services/login' 
 import BlogCreatorForm from './components/CreationForm'
-const NotificationBar = (props) => {
-  return(<div>
-    {props.user !== null && <p>User {props.user.name} logged in <button onClick={() => props.logoutfunc()}>Logout</button></p>}
-    {props.type === "Error" && <p style={{color: 'red',border: '1px solid red'}}>Invalid login credentials</p>}
-    {props.type === "BlogAdded" && <p style={{color: 'green',border: '1px solid green'}}>A new blog added: {props.blog.title} by {props.blog.author}</p>}
-  </div>)
-}
+import blogService from './services/blogs'
+import NotificationBar from './components/Notificationbar'
+
+
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
@@ -19,7 +15,7 @@ const App = () => {
   const [infoMessage, setInfoMessage] = useState(null) 
   const [password, setPassword] = useState('')
   const [currUser, setCurrUser] = useState(null)
-  const [newBlog,setNewBlog] = useState(null) 
+  
 
   // Uloskirjautuu sovelluksesta
   const logOut = () => {
@@ -46,44 +42,11 @@ const App = () => {
       setUsername('')
       setPassword('')
     } catch (exp) {
-      console.log('örf')
-      setInfoMessage("Error")
+      setInfoMessage({type: "Error"})
       setTimeout(() => {
         setInfoMessage(null)
       }, 3000)
     }
-  }
-
-  const blogBuilder = (value,id) => {
-
-    // eslint-disable-next-line default-case
-    switch (id) {
-      case "newBlogTitle":
-        setNewBlog({...newBlog,
-        title: value})
-        break;
-      case "newBlogAuthor":
-        setNewBlog({...newBlog,
-        author: value})
-        break;
-      case "newBlogURL":
-        setNewBlog({...newBlog,
-        url: value})
-        break;
-
-       
-    }
-
-  }
-
-  const createBlog = async (e) => {
-    e.preventDefault()
-    await blogService.createNew(newBlog)
-    setInfoMessage('BlogAdded')
-    setTimeout(() => {
-      setInfoMessage(null)
-      setNewBlog(null)
-    }, 3000)
   }
 
   useEffect(() => {
@@ -104,12 +67,12 @@ const App = () => {
   return (
     <div>
       <h2 style={{color:'purple'}}>BLOGS</h2>
-      {(currUser !== null || infoMessage !== null) && <NotificationBar user={currUser} type={infoMessage} blog={newBlog} logoutfunc={logOut}/>}
+      {(currUser !== null || infoMessage !== null) && <NotificationBar user={currUser} message={infoMessage} logoutfunc={logOut}/>}
       {currUser === null && 
       <LoginForm loginFunction={handleLogin} usernameFunction={setUsername} passwordFunction={setPassword}/>}
     {currUser !== null &&
       <Togglable>
-        <BlogCreatorForm changeValue={blogBuilder} blogCreateFunction={createBlog}/>
+        <BlogCreatorForm infoMSgFunction={setInfoMessage}/>
       </Togglable>
       }
       {currUser !== null &&
--- src/components/CreationForm.jsx
+++ src/components/CreationForm.jsx
@@ -1,24 +1,56 @@
-import React from 'react'
+import React, {useState} from 'react'
+import blogService from '../services/blogs'
 
+const BlogCreatorForm = ({infoMSgFunction}) => {
 
-const BlogCreatorForm = ({blogCreateFunction,changeValue}) => {
+    const [newBlog,setNewBlog] = useState(null) 
+    
+    
+    const createBlog = async (e) => {
+        e.preventDefault()
+        await blogService.createNew(newBlog)
+        infoMSgFunction({type:'BlogAdded',blog:newBlog})
+        setTimeout(() => {
+            infoMSgFunction(null)
+          setNewBlog(null)
+        }, 3000)
+      }
+    
+  const blogBuilder = (value,id) => {
 
+    // eslint-disable-next-line default-case
+    switch (id) {
+      case "newBlogTitle":
+        setNewBlog({...newBlog,
+        title: value})
+        break;
+      case "newBlogAuthor":
+        setNewBlog({...newBlog,
+        author: value})
+        break;
+      case "newBlogURL":
+        setNewBlog({...newBlog,
+        url: value})
+        break;
+    }
+
+  }
 return(
 <div>
     <h3 style={{color: 'purple'}}>Create New Blog</h3>
-    <form onSubmit={blogCreateFunction}>
+    <form onSubmit={createBlog}>
         <div>
             <div style={{margin: '10px'}}>
                 <label style={{display: 'inline-block',width:'100px'}}>Title</label>
-                <input onChange={({ target }) => changeValue(target.value,target.id)} id="newBlogTitle" type="text"></input>
+                <input onChange={({ target }) => blogBuilder(target.value,target.id)} id="newBlogTitle" type="text"></input>
             </div>
             <div style={{margin: '10px'}}>
                 <label style={{display: 'inline-block',width:'100px'}}>Author</label>
-                <input onChange={({ target }) => changeValue(target.value,target.id)} id="newBlogAuthor" type="text"></input>
+                <input onChange={({ target }) => blogBuilder(target.value,target.id)} id="newBlogAuthor" type="text"></input>
             </div>
             <div style={{margin: '10px'}}>
                 <label style={{display: 'inline-block',width:'100px'}}>URL</label>
-                <input onChange={({ target }) => changeValue(target.value,target.id)} id="newBlogURL" type="text"></input>
+                <input onChange={({ target }) => blogBuilder(target.value,target.id)} id="newBlogURL" type="text"></input>
             </div>
             <button style={{width:'80px',backgroundColor: 'green',color:'white'}} type="submit">Create</button>
         </div>
